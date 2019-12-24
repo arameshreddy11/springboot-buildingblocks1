6e76bed75ca6fd430097671b93df454ae774ec57
91767 <91767@DESKTOP-VP20DLR>
2019-12-24 10:58:37 +0530

    second

--- src/main/java/com/stacksimpfly/restservices1/springbootbuildingblocks1/SpringbootBuildingblocks1Application.java
+++ src/main/java/com/stacksimpfly/restservices1/springbootbuildingblocks1/SpringbootBuildingblocks1Application.java
@@ -2,7 +2,7 @@ package com.stacksimpfly.restservices1.springbootbuildingblocks1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootBuildingblocks1Application {
 
