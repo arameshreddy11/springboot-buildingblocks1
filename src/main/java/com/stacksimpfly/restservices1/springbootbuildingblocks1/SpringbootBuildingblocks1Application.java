0ffcc21055afdd0cceed8310a6964e1a387a6571
91767 <91767@DESKTOP-VP20DLR>
2019-12-24 10:57:00 +0530

    first

---
+++ src/main/java/com/stacksimpfly/restservices1/springbootbuildingblocks1/SpringbootBuildingblocks1Application.java
@@ -0,0 +1,13 @@
+package com.stacksimpfly.restservices1.springbootbuildingblocks1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocks1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocks1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimpfly/restservices1/springbootbuildingblocks1/SpringbootBuildingblocks1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimpfly.restservices1.springbootbuildingblocks1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocks1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
